f65387dc6ffbf093af205a19876559997ba41e99
AndreyZakourcev <andrey.z@nownodes.io>
2025-09-16 13:51:43 +0300

    add basket

--- 1_lab/script.js
+++ 1_lab/script.js
@@ -33,6 +33,33 @@ document.addEventListener('DOMContentLoaded', () => {
         productsContainer.appendChild(li);
     });
 
+    let cart = {};
+    
+    // Открытие/закрытие корзины
+    const cartModal = document.getElementById("cartModal");
+    document.getElementById("cartBtn").onclick = () => { showCart(); cartModal.classList.remove("hidden"); };
+    document.getElementById("closeCart").onclick = () => cartModal.classList.add("hidden");
+
+    // Показ содержимого корзины
+    function showCart() {
+    const cartItems = document.getElementById("cartItems");
+    cartItems.innerHTML = "";
+    let total = 0;
+    for (let id in cart) {
+        const item = cart[id];
+        total += item.price * item.qty;
+
+        const li = document.createElement("li");
+        li.innerHTML = `
+        ${item.title} - ${item.price} руб. × 
+        <input type="number" min="1" value="${item.qty}" data-id="${id}" class="qty-input">
+        = ${item.price * item.qty} руб.
+        <button class="remove-btn" data-id="${id}">Удалить</button>
+        `;
+        cartItems.appendChild(li);
+    }
+    document.getElementById("cartTotal").textContent = total;
+}
     } catch (err) {
         console.error('Ошибка в основном блоке script.js:', err);
     }
